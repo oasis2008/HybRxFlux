f142fefc5e3d638089510a23c65f0e640ad10b9e
coolfire2015 <coolfirefreedom@gmail.com>
2016-06-30 14:06:44 +0800

    defult

---
+++ app/src/main/java/com/huyingbao/hyb/ui/user/UserInfoAty.java
@@ -0,0 +1,13 @@
+package com.huyingbao.hyb.ui.user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserInfoAty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_user_info);
+    }
+}
