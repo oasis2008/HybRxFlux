5dfc147f27f066239f4bc7246942efc9dbd91a56
coolfire2015 <coolfirefreedom@gmail.com>
2016-07-21 15:13:07 +0800

    更新用户信息

--- app/src/main/java/com/huyingbao/hyb/inject/component/ActivityComponent.java
+++ app/src/main/java/com/huyingbao/hyb/inject/component/ActivityComponent.java
@@ -9,6 +9,10 @@ import com.huyingbao.hyb.inject.module.ActivityModule;
 import com.huyingbao.hyb.inject.qualifier.ContextLife;
 import com.huyingbao.hyb.inject.scope.PerActivity;
 import com.huyingbao.hyb.ui.login.LoginAty;
+import com.huyingbao.hyb.ui.login.RegisterAty;
+import com.huyingbao.hyb.ui.shop.RegisterShopAty;
+import com.huyingbao.hyb.ui.shop.ShopDetailAty;
+import com.huyingbao.hyb.ui.user.UserSendMessageAty;
 
 import dagger.Component;
 
@@ -35,8 +39,16 @@ public interface ActivityComponent {
 
     void inject(BaseActivity baseActivity);
 
-    void inject(MainAty MainAty);
+    void inject(LoginAty loginAty);
 
-    void inject(LoginAty MainAty);
+    void inject(MainAty mainAty);
+
+    void inject(UserSendMessageAty userSendMessageAty);
+
+    void inject(RegisterAty registerAty);
+
+    void inject(RegisterShopAty registerShopAty);
+
+    void inject(ShopDetailAty shopDetailAty);
 
 }
